c5acf747936a9a9703677c70fa9a7ba6122091ee
akash-svg <akash001karim@gmail.com>
2020-08-23 12:44:45 +0600

    change by Akash

---
+++ app/src/main/java/com/sfbd/serviceforcebd/activity/NotFound.java
@@ -0,0 +1,16 @@
+package com.sfbd.serviceforcebd.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sfbd.serviceforcebd.R;
+
+public class NotFound extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_not_found);
+    }
+}
