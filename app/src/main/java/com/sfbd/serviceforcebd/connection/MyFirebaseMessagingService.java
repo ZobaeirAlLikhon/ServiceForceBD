610ce80f20a3c1af04446dbdf9294a07c8f6687b
akash-svg <akash001karim@gmail.com>
2020-09-20 16:06:25 +0600

    Updated by likhon

--- app/src/main/java/com/sfbd/serviceforcebd/connection/MyFirebaseMessagingService.java
+++ app/src/main/java/com/sfbd/serviceforcebd/connection/MyFirebaseMessagingService.java
@@ -2,8 +2,13 @@ package com.sfbd.serviceforcebd.connection;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Build;
 import android.util.Log;
 
@@ -13,32 +18,45 @@ import androidx.core.app.NotificationCompat;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 import com.sfbd.serviceforcebd.R;
+import com.sfbd.serviceforcebd.activity.MainActivity;
 
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Map;
 import java.util.Random;
 
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
     private static final String TAG = "TOKEN";
-
+    Bitmap bitmap;
 //    private static final String TAG = "MyFirebaseMsgService";
 
 
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
 //        super.onMessageReceived(remoteMessage);
+        String imageUri = remoteMessage.getNotification().getImageUrl().toString();
+        bitmap =getBitmapfromUrl(imageUri);
         if(remoteMessage.getData().isEmpty())
         {
-            showNotification(remoteMessage.getNotification().getTitle(),remoteMessage.getNotification().getBody());
+
+
+            showNotification(remoteMessage.getNotification().getTitle(),remoteMessage.getNotification().getBody(),bitmap);
         }
         else{
-            showNotification(remoteMessage.getData());
+            showNotification(remoteMessage.getData(),bitmap);
+
         }
     }
-    public void showNotification(Map<String,String> data)
+    public void showNotification(Map<String,String> data,Bitmap bitmap)
     {
         String title=data.get("title").toString();
         String body=data.get("body").toString();
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         String notification_Channel_Id="SFBD";
@@ -60,12 +78,21 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         notificationBuilder.setSmallIcon(R.drawable.logo1);
         notificationBuilder.setContentTitle(title);
         notificationBuilder.setContentText(body);
+        notificationBuilder.setStyle(new NotificationCompat.BigPictureStyle()
+                .bigPicture(bitmap));
+        notificationBuilder.setLargeIcon(bitmap);
         notificationBuilder.setContentInfo("Info");
-        notificationManager.notify(new Random().nextInt(),notificationBuilder.build());
+        notificationBuilder.setContentIntent(pendingIntent);
+
+        notificationManager.notify(999, notificationBuilder.build());
     }
 
-    public void showNotification(String title,String body)
+    public void showNotification(String title, String body, Bitmap bitmap)
     {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         String notification_Channel_Id="SFBD";
@@ -87,14 +114,37 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         notificationBuilder.setSmallIcon(R.drawable.logo1);
         notificationBuilder.setContentTitle(title);
         notificationBuilder.setContentText(body);
+        notificationBuilder.setStyle(new NotificationCompat.BigPictureStyle()
+                .bigPicture(bitmap));
+        notificationBuilder.setLargeIcon(bitmap);
         notificationBuilder.setContentInfo("Info");
-        notificationManager.notify(new Random().nextInt(),notificationBuilder.build());
+        notificationBuilder.setContentIntent(pendingIntent);
+
+        notificationManager.notify(999, notificationBuilder.build());
 
     }
     @Override
     public void onNewToken(String token) {
         Log.d(TAG, "Refreshed token: " + token);
 
+    }
+    public Bitmap getBitmapfromUrl(String imageUrl) {
+        try {
+
+            URL url = new URL(imageUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap bitmap = BitmapFactory.decodeStream(input);
+            return bitmap;
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return null;
+
+        }
     }
 
 
