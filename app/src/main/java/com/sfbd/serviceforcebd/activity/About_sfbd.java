afa1fe38b4bbfcded78e4b1cce9d784004ed44ff
akash-svg <akash001karim@gmail.com>
2020-10-08 11:01:43 +0600

    New Design of sign up and sign in page

--- app/src/main/java/com/sfbd/serviceforcebd/activity/About_sfbd.java
+++ app/src/main/java/com/sfbd/serviceforcebd/activity/About_sfbd.java
@@ -2,10 +2,13 @@ package com.sfbd.serviceforcebd.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.sfbd.serviceforcebd.R;
 import com.sfbd.serviceforcebd.databinding.ActivityAboutSfbdBinding;
+import com.sfbd.serviceforcebd.fragment.MoreFragment;
 
 public class About_sfbd extends AppCompatActivity {
     ActivityAboutSfbdBinding binding;
@@ -14,5 +17,11 @@ public class About_sfbd extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding=ActivityAboutSfbdBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        binding.backBtnPro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              onBackPressed();
+            }
+        });
     }
 }
